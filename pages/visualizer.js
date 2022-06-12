eb7a5e6bf9eb99b1b87939ac5e997756354a9250
Sean Purcell <me@seanp.xyz>
2022-06-12 00:09:27 -0400

    Visualizer setup

--- pages/visualizer.js
+++ pages/visualizer.js
@@ -7,6 +7,8 @@ import styles from '../styles/Home.module.css'
 
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import Overlay from 'react-bootstrap/Overlay'
+import Tooltip from 'react-bootstrap/Tooltip'
 
 import Datetime from 'react-datetime'
 
@@ -17,147 +19,75 @@ import { faLocationPin } from '@fortawesome/free-solid-svg-icons'
 
 import axios from 'axios';
 
-function MapPin({}) {
-  return (
-    <FontAwesomeIcon icon={faLocationPin}/>
-  );
+function DeathTip({ death }) {
+  const target = useRef(null);
+
+  return (<div>
+  <div
+    ref={target}
+    style={{
+      position: 'absolute',
+      bottom: `calc(100% * (1 - ${death.pin.y}))`,
+      left: `calc(100% * (${death.pin.x}))`
+  }}
+
+  >
+    </div>
+  <Overlay show={true} target={target.current} shouldUpdatePosition={true} key={Math.random()}>
+  <Tooltip>
+  {death.desc}
+  </Tooltip>
+  </Overlay>
+    </div>);
 }
 
-function Map({ name, pinLocation, onClick }) {
-  function handle_click(e) {
-    e.preventDefault();
-    const bounds = e.target.getBoundingClientRect();
-    const offsetX = e.clientX - bounds.x;
-    const offsetY = e.clientY - bounds.y;
-    const x = offsetX / bounds.width;
-    const y = offsetY / bounds.height;
-    const val = { x, y };
-    console.log('Map click', offsetX, offsetY, val, e);
-
-    if (onClick) {
-      onClick(val);
-    }
-  }
+function Map({ name, death }) {
 
   const [ marker, setMarkers ] = useState([]);
-
-  const width = 0.02;
 
   const src = '/maps/' + name + '.png';
 
   return (
     <div className={styles.mapdiv}>
-      <img src={src} className={styles.map} onClick={handle_click}/>
-      <div style={{
-          position: 'absolute',
-          color: '#e63036d0',
-          bottom: `calc(100% * (1 - ${pinLocation.y}))`,
-          left: `calc(100% * (${pinLocation.x} - ${width}/2))`,
-          width: `calc(100% * ${width})` }}
-      >
-        <FontAwesomeIcon icon={faLocationPin}/>
-      </div>
+      <img src={src} className={styles.map}/>
+      {(death && <DeathTip death={death} />)}
     </div>
   );
 }
 
-function DeathForm({}) {
-  const [ desc, setDesc ] = useState('');
-  const [ curTime, setCurTime ] = useState(Date.now());
-  const [ pickedTime, setPickedTime ] = useState(null);
-  const [ pin, setPin ] = useState({ x: 0, y: 0 });
+function DeathForm({ deaths }) {
+  const [ index, setIndex ] = useState(0);
+  console.log('On death', index);
 
-  const maps = [ 'guide', 'limgrave', 'bestial_sanctum', 'caelid', 'caelid-2', 'weeping-peninsula', 'full-1', 'liurnia-1', 'ainsel-river-blind', 'ainsel-river', 'liurnia-2', 'liurnia-3', 'liurnia-4', 'liurnia-5', 'carian', 'siofra', 'dragonbarrow', 'altus-plateau', 'deeproot-depths', 'mt-gelmir', 'leyndell', 'giants-mountain-1', 'giants-mountain-2', 'flame-peak', 'consecrated-snowfield', 'haligtree', 'lake-of-rot', 'astel', 'farum-azula', 'siofra-full' ];
-  const [ map, setMap ] = useState(maps[0]);
-
-  var timeRef = useRef();
+  const maps = [ 'overworld', 'underworld' ];
 
   const dateFormat = "YYYY-MM-DD";
   const timeFormat = "hh:mm:ss A";
   const formatTime = (t) => moment(t).format(dateFormat + " " + timeFormat);
 
-  function updateTime(t) {
-    console.log('Updating time', t);
-    setPickedTime(t);
-  }
-
-  timeRef.current = pickedTime;
-
-  function handle_submit(e) {
-    e.preventDefault();
-
-    const curTime = formatTime(curTime);
-    const pickedTime = timeRef.current ? formatTime(timeRef.current) : null;
-    const time = pickedTime || curTime;
-    const values = {
-      desc, curTime, pickedTime, pin, time, map
-    };
-
-    console.log('Submitting form', values);
-
-    axios.post('/api/death', values).then((response) => {
-      console.log('Successfully submitted form', response);
-    }).catch((error) => {
-      console.error('Failed to submit form', error);
-    });
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurTime(Date.now());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  const death = (index < deaths.length && deaths[index]);
+  const map = (death && death.map) || 'overworld';
 
   return (
     <div>
-      <div className={styles.deathform}>
-        <Form onSubmit={handle_submit}>
-          <Form.Group className="mb-3" controlId="desc">
-            <Form.Control type="text" placeholder="Description" value={desc} onChange={(e) => setDesc(e.target.value)}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="time">
-            <Datetime
-              value={pickedTime ? moment(pickedTime).toDate() : null}
-              inputProps={{ placeholder: formatTime(curTime) }}
-              timeFormat={timeFormat}
-              dateFormat={dateFormat}
-              onChange={updateTime} />
-          </Form.Group>
-          <div className="row">
-            <div className="col">
-              <Form.Group className="mb-3 row" controlId="x">
-                <Form.Label className="col-1" style={{ margin: 'auto' }}>x</Form.Label>
-                <Form.Control className="col" type="text" readOnly disabled value={pin.x}/>
-              </Form.Group> 
-            </div>
-            <div className="col">
-              <Form.Group className="mb-3 row" controlId="y">
-                <Form.Label className="col-1" style={{ margin: 'auto' }}>y</Form.Label>
-                <Form.Control className="col" type="text" readOnly disabled value={pin.y}/>
-              </Form.Group> 
-            </div>
-          </div>
-          <div className="form-check mb-3 row">
-            {
-              maps.map((option) => (
-                <Form.Check key={option} type='radio' name='map-selector' value={option} label={option} onClick={() => setMap(option)}/>
-              ))
-            }
-          </div>
-          <div className="row">
-            <div className="col-sm">
-              <Button variant="primary" type="submit">Death</Button>
-            </div>
-          </div>
-        </Form>
-      </div>
-      <Map pinLocation={pin} onClick={setPin} name={map} />
+      <Form.Range id='index' value={index} onChange={(e) => setIndex(e.target.value)} min={0} max={deaths.length-1}/>
+        <Map name={map} death={death} />
     </div>
   );
 }
 
 export default function Home() {
+  const [ deaths, setDeaths ] = useState([]);
+
+  useEffect(() => {
+    axios.get('/data/deaths-stitched.json').then((response) => {
+      console.log('Fetched deaths', response);
+      setDeaths(response.data.split('\n').slice(0,-1).map(JSON.parse))
+    }).catch((error) => {
+      console.error('Failed to fetch data', error);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -166,7 +96,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <DeathForm/>
+      <DeathForm deaths={deaths}/>
     </div>
   )
 }
